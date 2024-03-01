f898550e59c400231dec574cbb4f68cb2ddc5a64
Jeffrey Green <elijahthehero@aol.com>
2024-03-02 00:19:23 +0100

    Commented out insert values. In task table change priority and status to varchar.

--- taskmanagement.sql
+++ taskmanagement.sql
@@ -22,18 +22,16 @@
 DROP TABLE IF EXISTS `tasks`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `tasks`
-(
-    `taskId`      int          NOT NULL AUTO_INCREMENT,
-    `title`       varchar(255) NOT NULL,
-    `description` varchar(255)      DEFAULT NULL,
-    `dueDate`     timestamp    NULL DEFAULT CURRENT_TIMESTAMP,
-    `priority`    tinyint(1)        DEFAULT NULL,
-    `status`      tinyint(1)        DEFAULT '0',
-    `userId`      int               DEFAULT NULL,
-    PRIMARY KEY (`taskId`),
-    KEY           `FK_usertask`(`userId`
-) ,
+CREATE TABLE `tasks` (
+  `taskId` int NOT NULL AUTO_INCREMENT,
+  `title` varchar(255) NOT NULL,
+  `description` varchar(255) DEFAULT NULL,
+  `dueDate` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `priority` varchar(255) DEFAULT NULL,
+  `status` varchar(255) DEFAULT NULL,
+  `userId` int DEFAULT NULL,
+  PRIMARY KEY (`taskId`),
+  KEY `FK_usertask` (`userId`),
   CONSTRAINT `FK_usertask` FOREIGN KEY (`userId`) REFERENCES `users` (`userId`)
 ) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
@@ -44,9 +42,7 @@ CREATE TABLE `tasks`
 
 LOCK TABLES `tasks` WRITE;
 /*!40000 ALTER TABLE `tasks` DISABLE KEYS */;
-INSERT INTO `tasks`
-VALUES (1, 'title', '', '2021-01-24 15:41:44', 1, 0, 101000),
-       (2, 'title2', '', '2024-01-27 16:41:44', 1, 0, 101001);
+-- INSERT INTO `tasks` VALUES (1,'title','','2021-01-24 15:41:44',1,0,101000),(2,'title2','','2024-01-27 16:41:44',1,0,101001);
 /*!40000 ALTER TABLE `tasks` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -57,14 +53,13 @@ UNLOCK TABLES;
 DROP TABLE IF EXISTS `users`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `users`
-(
-    `userId`   int          NOT NULL AUTO_INCREMENT,
-    `username` varchar(255) NOT NULL,
-    `password` varchar(255) NOT NULL,
-    `email`    varchar(255)      DEFAULT NULL,
-    `signDate` timestamp    NULL DEFAULT CURRENT_TIMESTAMP,
-    PRIMARY KEY (`userId`)
+CREATE TABLE `users` (
+  `userId` int NOT NULL AUTO_INCREMENT,
+  `username` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  `signDate` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`userId`)
 ) ENGINE=InnoDB AUTO_INCREMENT=101016 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -74,23 +69,7 @@ CREATE TABLE `users`
 
 LOCK TABLES `users` WRITE;
 /*!40000 ALTER TABLE `users` DISABLE KEYS */;
-INSERT INTO `users`
-VALUES (101000, 'test1', '5a105e8b9d40e1329780d62ea2265d8a', '101000@email.com', '2024-01-22 02:16:00'),
-       (101001, 'test2', 'ad0234829205b9033196ba818f7a872b', NULL, '2024-01-22 02:16:00'),
-       (101002, 'test3', '8ad8757baa8564dc136c1e07507f4a98', NULL, '2024-01-22 02:16:00'),
-       (101003, 'test4', '86985e105f79b95d6bc918fb45ec7727', NULL, '2024-01-22 02:16:00'),
-       (101004, 'test5', 'e3d704f3542b44a621ebed70dc0efe13', NULL, '2024-01-22 02:16:00'),
-       (101005, 'test6', '4cfad7076129962ee70c36839a1e3e15', NULL, '2024-01-22 02:16:00'),
-       (101006, 'test7', 'b04083e53e242626595e2b8ea327e525', NULL, '2024-01-22 02:16:00'),
-       (101007, 'test8', '5e40d09fa0529781afd1254a42913847', NULL, '2024-01-22 02:16:00'),
-       (101008, 'test9', '739969b53246b2c727850dbb3490ede6', NULL, '2024-01-22 02:16:00'),
-       (101009, 'test10', 'c1a8e059bfd1e911cf10b626340c9a54', NULL, '2024-01-22 02:16:00'),
-       (101010, 'test11', 'f696282aa4cd4f614aa995190cf442fe', NULL, '2024-01-22 02:16:00'),
-       (101011, 'test12', '60474c9c10d7142b7508ce7a50acf414', NULL, '2024-01-22 02:16:00'),
-       (101012, 'test13', '33fc3dbd51a8b38a38b1b85b6a76b42b', NULL, '2024-01-22 02:16:00'),
-       (101013, 'test14', 'b99c94f62fb2a61433c4e44e27406050', NULL, '2024-01-22 02:16:00'),
-       (101014, 'test15', '4b377d23309d4ed39c9da5791417aeff', NULL, '2024-01-22 02:16:00'),
-       (101015, 'a', 'b', NULL, '2024-01-27 16:33:13');
+-- INSERT INTO `users` VALUES (101000,'test1','5a105e8b9d40e1329780d62ea2265d8a','101000@email.com','2024-01-22 02:16:00'),(101001,'test2','ad0234829205b9033196ba818f7a872b',NULL,'2024-01-22 02:16:00'),(101002,'test3','8ad8757baa8564dc136c1e07507f4a98',NULL,'2024-01-22 02:16:00'),(101003,'test4','86985e105f79b95d6bc918fb45ec7727',NULL,'2024-01-22 02:16:00'),(101004,'test5','e3d704f3542b44a621ebed70dc0efe13',NULL,'2024-01-22 02:16:00'),(101005,'test6','4cfad7076129962ee70c36839a1e3e15',NULL,'2024-01-22 02:16:00'),(101006,'test7','b04083e53e242626595e2b8ea327e525',NULL,'2024-01-22 02:16:00'),(101007,'test8','5e40d09fa0529781afd1254a42913847',NULL,'2024-01-22 02:16:00'),(101008,'test9','739969b53246b2c727850dbb3490ede6',NULL,'2024-01-22 02:16:00'),(101009,'test10','c1a8e059bfd1e911cf10b626340c9a54',NULL,'2024-01-22 02:16:00'),(101010,'test11','f696282aa4cd4f614aa995190cf442fe',NULL,'2024-01-22 02:16:00'),(101011,'test12','60474c9c10d7142b7508ce7a50acf414',NULL,'2024-01-22 02:16:00'),(101012,'test13','33fc3dbd51a8b38a38b1b85b6a76b42b',NULL,'2024-01-22 02:16:00'),(101013,'test14','b99c94f62fb2a61433c4e44e27406050',NULL,'2024-01-22 02:16:00'),(101014,'test15','4b377d23309d4ed39c9da5791417aeff',NULL,'2024-01-22 02:16:00'),(101015,'a','b',NULL,'2024-01-27 16:33:13');
 /*!40000 ALTER TABLE `users` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
