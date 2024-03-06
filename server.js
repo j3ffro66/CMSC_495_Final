7e25a442b3ad4c9f71b3e7885447eabd5a5c329f
Jeff <elijahthehero@aol.com>
2024-03-06 04:52:46 +0100

    Implement https.

--- server.js
+++ server.js
@@ -38,9 +38,9 @@ const options = {
 };
 
 // Retrieve the port number from environment variables with a fallback to 3000 if not specified
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 443;
 // Initialize server
 const server = http.createServer(options, app);
 
 // Start the server and listen on the specified port, logging a message to the console when the server is ready
-server.listen(process.env.PORT || 3000, () => console.log(`Server running on port ${PORT}`));
+server.listen(process.env.PORT || 443, () => console.log(`Server running on port ${PORT}`));
